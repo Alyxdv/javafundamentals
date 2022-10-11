3e0745a63b165daf25101c456d2a212b5dfc6d52
Alyxdv <alyxdv@gmail.com>
2022-10-10 20:53:12 -0500

    Lesson 2 - My Application Overview

--- JavaFundamentals/src/com/fundamentals/java/Main.java
+++ JavaFundamentals/src/com/fundamentals/java/Main.java
@@ -3,7 +3,33 @@ package com.fundamentals.java;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        //System.out.println("Hello Java");
+        //houseExample();
+        basicLab();
     } // end method
+/*
+* This is just me trying out the multi-lined comment.
+* */
+    public static void houseExample() {
+        House myHouse = new House();
+        myHouse.doorColor = "Red";
+        myHouse.doorFunction();
 
+        House otherHouse = new House();
+        otherHouse.doorColor = "Yellow";
+
+        System.out.println(myHouse.doorColor);
+        System.out.println(otherHouse.doorColor);
+
+        House thirdHouse = otherHouse;
+        thirdHouse.doorColor = "Blue";
+
+        System.out.println(thirdHouse.doorColor);
+        System.out.println(otherHouse.doorColor);
+
+    public static void basicLab() {
+
+
+
+    }
 } // end class
