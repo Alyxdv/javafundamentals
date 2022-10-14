6542559ebdacc820a66ca60b693d5e47ede41cef
Alyxdv <alyxdv@gmail.com>
2022-10-13 22:23:32 -0500

    Numbers HW 1st task

--- JavaFundamentals/src/com/fundamentals/labs/LabsMain.java
+++ JavaFundamentals/src/com/fundamentals/labs/LabsMain.java
@@ -5,13 +5,23 @@ public class LabsMain {
     public static void main(String[] args) {
 
         // call things here
-        StringsExamples();
+        //StringsExamples();
+        NumbersExamples();
+
     }
 
-    public static void StringsExamples() {
-        StringsLab examples = new StringsLab();
-        examples.labStrings();
-        examples.secondMethod();
+    public static void NumbersExamples() {
+
+        NumbersLab examples = new NumbersLab();
+        examples.numbersOne();
+
     }
 }
+
+
+    //public static void StringsExamples() {
+        //StringsLab examples = new StringsLab();
+        //examples.labStrings();
+        //examples.secondMethod();
+    //}
 
---
+++ JavaFundamentals/src/com/fundamentals/labs/NumbersLab.java
@@ -0,0 +1,19 @@
+package com.fundamentals.labs;
+
+public class NumbersLab {
+
+    int taskBinary = 0b11101;
+    int taskHex = 0x1D;
+    int taskOct = 035;
+
+    public void numbersOne() {
+
+        System.out.println("Binary = " + taskBinary);
+        System.out.println("Hexadecimal = " + taskHex);
+        System.out.println("Octal = " + taskOct);
+
+    }
+
+
+
+}
